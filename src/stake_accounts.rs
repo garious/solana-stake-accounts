dbacfa9eb43c378c7f933a66ad66af225f67932b
Greg Fitzgerald <greg@solana.com>
2020-03-30 09:10:37 -0600

    Add unit-tests

--- src/stake_accounts.rs
+++ src/stake_accounts.rs
@@ -101,7 +101,7 @@ fn move_stake_account(
         stake_authority_pubkey,
         lamports,
         &new_stake_account_address,
-        new_stake_authority_pubkey,
+        new_base_pubkey,
         &i.to_string(),
     );
 
@@ -175,33 +175,62 @@ mod tests {
     use super::*;
     use solana_runtime::{bank::Bank, bank_client::BankClient};
     use solana_sdk::{
+        account::Account,
         client::SyncClient,
         genesis_config::create_genesis_config,
         signature::{Keypair, Signer},
     };
     use solana_stake_program::stake_state::StakeState;
 
-    fn create_bank(lamports: u64) -> (Bank, Keypair) {
+    fn create_bank(lamports: u64) -> (Bank, Keypair, u64) {
         let (genesis_config, mint_keypair) = create_genesis_config(lamports);
         let mut bank = Bank::new(&genesis_config);
         bank.add_instruction_processor(
             solana_stake_program::id(),
             solana_stake_program::stake_instruction::process_instruction,
         );
-        (bank, mint_keypair)
+        let rent = bank.get_minimum_balance_for_rent_exemption(std::mem::size_of::<StakeState>());
+        (bank, mint_keypair, rent)
+    }
+
+    fn create_account<C: SyncClient>(
+        client: &C,
+        sender_keypair: &Keypair,
+        lamports: u64,
+    ) -> Keypair {
+        let fee_payer_keypair = Keypair::new();
+        client
+            .transfer(lamports, &sender_keypair, &fee_payer_keypair.pubkey())
+            .unwrap();
+        fee_payer_keypair
+    }
+
+    fn get_account_at<C: SyncClient>(client: &C, base_pubkey: &Pubkey, i: usize) -> Account {
+        let account_address = derive_stake_account_address(&base_pubkey, i);
+        client.get_account(&account_address).unwrap().unwrap()
+    }
+
+    fn get_balances<C: SyncClient>(
+        client: &C,
+        base_pubkey: &Pubkey,
+        num_accounts: usize,
+    ) -> Vec<(Pubkey, u64)> {
+        (0..num_accounts)
+            .into_iter()
+            .map(|i| {
+                let address = derive_stake_account_address(&base_pubkey, i);
+                (address, client.get_balance(&address).unwrap())
+            })
+            .collect()
     }
 
     #[test]
-    fn test_new_stake_account() {
-        let (bank, sender_keypair) = create_bank(10_000_000);
-        let rent = bank.get_minimum_balance_for_rent_exemption(std::mem::size_of::<StakeState>());
+    fn test_new_derived_stake_account() {
+        let (bank, sender_keypair, rent) = create_bank(10_000_000);
         let sender_pubkey = sender_keypair.pubkey();
         let bank_client = BankClient::new(bank);
-        let fee_payer_keypair = Keypair::new();
+        let fee_payer_keypair = create_account(&bank_client, &sender_keypair, 1);
         let fee_payer_pubkey = fee_payer_keypair.pubkey();
-        bank_client
-            .transfer(1, &sender_keypair, &fee_payer_pubkey)
-            .unwrap();
 
         let base_keypair = Keypair::new();
         let base_pubkey = base_keypair.pubkey();
@@ -220,5 +249,129 @@ mod tests {
 
         let signers = [&sender_keypair, &fee_payer_keypair, &base_keypair];
         bank_client.send_message(&signers, message).unwrap();
+
+        let account = get_account_at(&bank_client, &base_pubkey, 0);
+        assert_eq!(account.lamports, lamports);
+        let authorized = StakeState::authorized_from(&account).unwrap();
+        assert_eq!(authorized.staker, stake_authority_pubkey);
+        assert_eq!(authorized.withdrawer, withdraw_authority_pubkey);
+    }
+
+    #[test]
+    fn test_authorize_stake_accounts() {
+        let (bank, sender_keypair, rent) = create_bank(10_000_000);
+        let sender_pubkey = sender_keypair.pubkey();
+        let bank_client = BankClient::new(bank);
+        let fee_payer_keypair = create_account(&bank_client, &sender_keypair, 1);
+        let fee_payer_pubkey = fee_payer_keypair.pubkey();
+
+        let base_keypair = Keypair::new();
+        let base_pubkey = base_keypair.pubkey();
+        let lamports = rent + 1;
+
+        let stake_authority_keypair = Keypair::new();
+        let stake_authority_pubkey = stake_authority_keypair.pubkey();
+        let withdraw_authority_keypair = Keypair::new();
+        let withdraw_authority_pubkey = withdraw_authority_keypair.pubkey();
+
+        let message = new_stake_account(
+            &fee_payer_pubkey,
+            &sender_pubkey,
+            &base_pubkey,
+            lamports,
+            &stake_authority_pubkey,
+            &withdraw_authority_pubkey,
+        );
+
+        let signers = [&sender_keypair, &fee_payer_keypair, &base_keypair];
+        bank_client.send_message(&signers, message).unwrap();
+
+        let new_stake_authority_pubkey = Pubkey::new_rand();
+        let new_withdraw_authority_pubkey = Pubkey::new_rand();
+        let messages = authorize_stake_accounts(
+            &fee_payer_pubkey,
+            &base_pubkey,
+            &stake_authority_pubkey,
+            &withdraw_authority_pubkey,
+            &new_stake_authority_pubkey,
+            &new_withdraw_authority_pubkey,
+            1,
+        );
+
+        let signers = [
+            &fee_payer_keypair,
+            &stake_authority_keypair,
+            &withdraw_authority_keypair,
+        ];
+        for message in messages {
+            bank_client.send_message(&signers, message).unwrap();
+        }
+
+        let account = get_account_at(&bank_client, &base_pubkey, 0);
+        let authorized = StakeState::authorized_from(&account).unwrap();
+        assert_eq!(authorized.staker, new_stake_authority_pubkey);
+        assert_eq!(authorized.withdrawer, new_withdraw_authority_pubkey);
+    }
+
+    #[test]
+    fn test_move_stake_accounts() {
+        let (bank, sender_keypair, rent) = create_bank(10_000_000);
+        let sender_pubkey = sender_keypair.pubkey();
+        let bank_client = BankClient::new(bank);
+        let fee_payer_keypair = create_account(&bank_client, &sender_keypair, 1);
+        let fee_payer_pubkey = fee_payer_keypair.pubkey();
+
+        let base_keypair = Keypair::new();
+        let base_pubkey = base_keypair.pubkey();
+        let lamports = rent + 1;
+
+        let stake_authority_keypair = Keypair::new();
+        let stake_authority_pubkey = stake_authority_keypair.pubkey();
+        let withdraw_authority_keypair = Keypair::new();
+        let withdraw_authority_pubkey = withdraw_authority_keypair.pubkey();
+
+        let num_accounts = 1;
+        let message = new_stake_account(
+            &fee_payer_pubkey,
+            &sender_pubkey,
+            &base_pubkey,
+            lamports,
+            &stake_authority_pubkey,
+            &withdraw_authority_pubkey,
+        );
+
+        let signers = [&sender_keypair, &fee_payer_keypair, &base_keypair];
+        bank_client.send_message(&signers, message).unwrap();
+
+        let new_base_keypair = Keypair::new();
+        let new_base_pubkey = new_base_keypair.pubkey();
+        let new_stake_authority_pubkey = Pubkey::new_rand();
+        let new_withdraw_authority_pubkey = Pubkey::new_rand();
+        let balances = get_balances(&bank_client, &base_pubkey, num_accounts);
+        let messages = move_stake_accounts(
+            &fee_payer_pubkey,
+            &new_base_pubkey,
+            &stake_authority_pubkey,
+            &withdraw_authority_pubkey,
+            &new_stake_authority_pubkey,
+            &new_withdraw_authority_pubkey,
+            &balances,
+        );
+        assert_eq!(messages.len(), num_accounts);
+
+        let signers = [
+            &fee_payer_keypair,
+            &new_base_keypair,
+            &stake_authority_keypair,
+            &withdraw_authority_keypair,
+        ];
+        for message in messages {
+            bank_client.send_message(&signers, message).unwrap();
+        }
+
+        let account = get_account_at(&bank_client, &new_base_pubkey, 0);
+        let authorized = StakeState::authorized_from(&account).unwrap();
+        assert_eq!(authorized.staker, new_stake_authority_pubkey);
+        assert_eq!(authorized.withdrawer, new_withdraw_authority_pubkey);
     }
 }
